e0f548425450923b65aab4c54758457371aaa80b
Dormierian Citizen <dormieriancitizen@gmail.com>
2025-10-24 19:51:23 -0400

    Merge scripts

--- icon-pack-recolorer/reloadtheme.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-/usr/lib/plasma-changeicons hicolor
-/usr/lib/plasma-changeicons Dormierian-Tela
--- icon-pack-recolorer/run.sh
+++ icon-pack-recolorer/run.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR" || exit 1
 
-. tela-install.sh
+ICON_DIR="$HOME/.cache/icon-pack-recolorer/tela-source"
+REPO_URL="https://github.com/vinceliuice/Tela-icon-theme.git"
 
-go build
+if [ ! -d "$ICON_DIR/.git" ]; then
+    echo "Directory not found, cloning repository..."
+    git clone "$REPO_URL" "$ICON_DIR"
+else
+    echo "Directory exists, updating repository..."
+    git -C "$ICON_DIR" pull
+fi
+
+$HOME/.cache/icon-pack-recolorer/tela-source/install.sh -d /tmp/tela-out nord
+
+echo "Updating tela-frappe"
+rm -rf $HOME/.local/share/icons/Tela-frappe/
+cp -r /tmp/tela-out/Tela-nord/ $HOME/.local/share/icons/Tela-frappe
+rm -rf /tmp/tela-out/
+
 echo "Recoloring target"
+go build
 ./icon-pack-recolorer $HOME/.local/share/icons/Tela-frappe/
 
 echo "Reloading theme"
-. reloadtheme.sh
+
+/usr/lib/plasma-changeicons hicolor
+/usr/lib/plasma-changeicons Dormierian-Tela
